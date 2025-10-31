deb6fa3bf239d29ca123b115485f182be28246b7
Márcio Nogueira <88255519+omarcionogueira@users.noreply.github.com>
2025-10-31 09:28:17 -0400

    Add Cadastro route to routing configuration

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Cadastro from './pages/Cadastro';
 
 function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/cadastro" element={<Cadastro />} />
     </Routes>
   );
 }
