00b60d677ae7a1d940a7b5b7e65cf9048333cab6
Márcio Nogueira <88255519+omarcionogueira@users.noreply.github.com>
2025-10-31 09:25:06 -0400

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <h2>Bem-vindo ao sistema</h2>
+      <p>Você está logado com sucesso.</p>
+    </div>
+  );
+}
+
+export default Home;
